92a8e7bfeeb591674027fedfc7abeb5356c8453b
DVBets <support@soroka-da.ru>
2025-11-17 02:25:32 +0300

    fix otg support

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -502,7 +502,7 @@
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  *
  */
-#define TEMP_SENSOR_0 0
+#define TEMP_SENSOR_0 13
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -517,7 +517,6 @@
 #define TEMP_SENSOR_BOARD 0
 #define TEMP_SENSOR_REDUNDANT 0
 
-#define DISABLE_TEMP_SENSOR_CHECK
 #define STARTUP_COMMANDS "G28"
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
--- Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3_common.h
+++ Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3_common.h
@@ -29,7 +29,7 @@
 // #define MKS_TEST
 
 // USB Flash Drive support
-//#define HAS_OTG_USB_HOST_SUPPORT
+#define HAS_OTG_USB_HOST_SUPPORT
 
 // Avoid conflict with TIMER_TONE
 #define STEP_TIMER                            10
