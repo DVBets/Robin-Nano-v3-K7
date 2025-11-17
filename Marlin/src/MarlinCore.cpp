e6103721bc495e644b5375aaff00f8f4b3471541
DVBets <support@soroka-da.ru>
2025-11-18 02:59:48 +0300

    Selftest
    Selftesting for mosfet, coolers and hotend

--- Marlin/src/MarlinCore.cpp
+++ Marlin/src/MarlinCore.cpp
@@ -584,7 +584,7 @@ inline void manage_inactivity(const bool no_stepper_sleep=false) {
     
               card.mount();
               if (!card.isMounted()) {
-                set_print_led_mode(PRINT_LED_OFF);
+                set_print_led_mode(PRINT_LED_ON);
                 print_key_flag = PF_START;
                 break;
               }
@@ -623,8 +623,17 @@ inline void manage_inactivity(const bool no_stepper_sleep=false) {
         }
         else {
           if (print_key_flag == PF_START && !printingIsActive()) {
-            set_print_led_mode(PRINT_LED_ON);
-            queue.inject_P(PSTR("G91\nG0 Z10 F600\nG90"));
+            // Run a simple heater and fan self-test to help confirm hardware health when no print is active
+            set_print_led_mode(PRINT_LED_BLINK_FAST);
+            queue.inject_P(PSTR(
+              "M106 P0 S255\n"   // Part-cooling fan full speed
+              "M106 S255\n"      // Hotend/board fan full speed
+              "M104 S150\n"      // Preheat hotend to 150C
+              "G4 S15\n"         // Hold temperature for 15 seconds
+              "M104 S0\n"        // Turn off hotend
+              "M106 P0 S0\n"     // Stop part-cooling fan
+              "M106 S0"          // Stop hotend/board fan
+            ));
           }
           else {
             card.abortFilePrintSoon();
